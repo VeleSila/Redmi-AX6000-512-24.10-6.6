4ab5318848f8c0c5cd37450643dd8f2f9c0c8764
VeleSila <VeleSila@users.noreply.github.com>
2026-03-27 23:05:32 -0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,17 +34,17 @@ sed -i "/set wireless.default_\${dev}.encryption='sae-mixed'/a \\\t\t\t\t\t\set 
 # 24.10-5.4内核：
 # sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
 # 24.10-6.6内核：名字看似是ubootmod，实则做了分区修改可以uboot放心刷入
-sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts
+sed -i 's/reg = <0x600000 0x[0-9a-fA-F]\{7\}>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000-ubootmod.dts
 
 # Theme
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 git clone https://github.com/sbwml/luci-theme-argon -b openwrt-24.10 package/argon
-git clone https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
+#git clone https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
 # 网速测试
-git clone https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
+#git clone https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
 # 主题高级设置
-git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
+#git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
 # adguardhome
 git clone https://github.com/F-57/luci-app-adguardhome package/luci-app-adguardhome
 # 安装 mosdns
@@ -54,14 +54,14 @@ rm -rf package/feeds/packages/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf package/feeds/packages/v2ray-geodata
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 安装 luci-app-openlist2 
-git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+#git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 # 安装隔空播放luci-app-airconnect
-git clone https://github.com/sbwml/luci-app-airconnect package/airconnect
+#git clone https://github.com/sbwml/luci-app-airconnect package/airconnect
 # 安装lucky
-git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
+#git clone https://github.com/sirpdboy/luci-app-lucky package/lucky
 # 安装 OpenClash
 git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
 rm -rf feeds/luci/applications/luci-app-openclash
@@ -71,14 +71,14 @@ mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
 echo -e "\nmsgid \"OpenClash\"" >> feeds/luci/applications/luci-app-openclash/po/zh-cn/openclash.zh-cn.po
 echo -e "msgstr \"科学上网\"" >> feeds/luci/applications/luci-app-openclash/po/zh-cn/openclash.zh-cn.po
 
-echo -e "\nmsgid \"MosDNS\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
-echo -e "msgstr \"转发分流\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+#echo -e "\nmsgid \"MosDNS\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+#echo -e "msgstr \"转发分流\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
 
-echo -e "\nmsgid \"Lucky\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
-echo -e "msgstr \"大吉大利\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
+#echo -e "\nmsgid \"Lucky\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
+#echo -e "msgstr \"大吉大利\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
 
-echo -e "\nmsgid \"OpenList\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
-echo -e "msgstr \"聚合网盘\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
+#echo -e "\nmsgid \"OpenList\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
+#echo -e "msgstr \"聚合网盘\"" >> package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po
 
 echo -e "\nmsgid \"UPnP IGD & PCP\"" >> feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po
 echo -e "msgstr \"即插即用\"" >> feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po
